c693db41ff986604dd5750a380e7b977b2e15222
holasebas <danielpalma2003@hotmail.com>
2021-02-08 20:35:35 -0500

    updated typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,58 +1,58 @@
 declare module "tnai" {
-    export default class Client {
+    export default class TnaiAPI {
         constructor(token: string);
         sfw: sfw;
         // hentai: nsfw;
         // real: real;
     }
     export interface sfw {
-        kiss(): Promise<string>;
-        hug(): Promise<string>;
-        kick(): Promise<string>;
-        slap(): Promise<string>;
-        dance(): Promise<string>;
-        happy(): Promise<string>;
-        confused(): Promise<string>;
-        angry(): Promise<string>;
-        cry(): Promise<string>;
-        blush(): Promise<string>;
-        sleep(): Promise<string>;
-        laugh(): Promise<string>;
-        feed(): Promise<string>;
-        pat(): Promise<string>;
-        bored(): Promise<string>;
-        greeting(): Promise<string>;
-        sad(): Promise<string>;
-        think(): Promise<string>;
-    };
+        kiss(queryParams?: string): Promise<string>;
+        hug(queryParams?: string): Promise<string>;
+        kick(queryParams?: string): Promise<string>;
+        slap(queryParams?: string): Promise<string>;
+        dance(queryParams?: string): Promise<string>;
+        happy(queryParams?: string): Promise<string>;
+        confused(queryParams?: string): Promise<string>;
+        angry(queryParams?: string): Promise<string>;
+        cry(queryParams?: string): Promise<string>;
+        blush(queryParams?: string): Promise<string>;
+        sleep(queryParams?: string): Promise<string>;
+        laugh(queryParams?: string): Promise<string>;
+        feed(queryParams?: string): Promise<string>;
+        pat(queryParams?: string): Promise<string>;
+        bored(queryParams?: string): Promise<string>;
+        greeting(queryParams?: string): Promise<string>;
+        sad(queryParams?: string): Promise<string>;
+        think(queryParams?: string): Promise<string>;
+    }
     export interface hentai {
-        boobs(): Promise<string>;
-        anal(): Promise<string>;
-        boobs(): Promise<string>;
-        blowjob(): Promise<string>;
-        pussy(): Promise<string>;
-        cum(): Promise<string>;
-        yuri(): Promise<string>;
-        double(): Promise<string>;
-        gif(): Promise<string>;
-        futa(): Promise<string>;
-        yaoi(): Promise<string>;
-        neko(): Promise<string>;
-        femdom(): Promise<string>;
-        pokemon(): Promise<string>;
-        yiff(): Promise<string>;
+        boobs(queryParams?: string): Promise<string>;
+        anal(queryParams?: string): Promise<string>;
+        boobs(queryParams?: string): Promise<string>;
+        blowjob(queryParams?: string): Promise<string>;
+        pussy(queryParams?: string): Promise<string>;
+        cum(queryParams?: string): Promise<string>;
+        yuri(queryParams?: string): Promise<string>;
+        double(queryParams?: string): Promise<string>;
+        gif(queryParams?: string): Promise<string>;
+        futa(queryParams?: string): Promise<string>;
+        yaoi(queryParams?: string): Promise<string>;
+        neko(queryParams?: string): Promise<string>;
+        femdom(queryParams?: string): Promise<string>;
+        pokemon(queryParams?: string): Promise<string>;
+        yiff(queryParams?: string): Promise<string>;
     }
     export interface real {
-        ass(): Promise<string>;
-        pussy(): Promise<string>;
-        boobs(): Promise<string>;
-        cosplay(): Promise<string>;
-        porngif(): Promise<string>;
-        cosplay(): Promise<string>;
-        bdsm(): Promise<string>;
-        black(): Promise<string>;
-        traps(): Promise<string>;
-        lesbians(): Promise<string>;
-        pawg(): Promise<string>;
+        ass(queryParams?: string): Promise<string>;
+        pussy(queryParams?: string): Promise<string>;
+        boobs(queryParams?: string): Promise<string>;
+        cosplay(queryParams?: string): Promise<string>;
+        porngif(queryParams?: string): Promise<string>;
+        cosplay(queryParams?: string): Promise<string>;
+        bdsm(queryParams?: string): Promise<string>;
+        black(queryParams?: string): Promise<string>;
+        traps(queryParams?: string): Promise<string>;
+        lesbians(queryParams?: string): Promise<string>;
+        pawg(queryParams?: string): Promise<string>;
     }
 }
